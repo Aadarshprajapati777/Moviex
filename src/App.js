3cbd491e56322680693b43cf0cb299a9c2dd61c0
Aadarsh <aadarsh9811594152@gmail.com>
2022-12-15 21:21:26 +0530

    search icon onclick function not working

--- src/App.js
+++ src/App.js
@@ -1,21 +1,58 @@
-import { useEffect } from "react";
+import { useEffect, useState} from "react";
+import './App.css';
+import SearchIcon from './search.svg';
+import MovieCard from "./MovieCard";
 
 const API_URL='http://www.omdbapi.com/?apikey=1856286e';
+   
+
+
+
+
 const App=()=>{
+    const[movies,setmovies]=useState([])
+    const[searchTerm, setsearchTerm]=useState("");
+
  const searchmovies=async(title)=>{
     const response= await fetch(`${API_URL}&s={title}`);
     const data= await response.json();
-    console.log(data.Search);
+    setmovies(data.Search);
  }
 
  useEffect(()=>{
-searchmovies('Spiderman')
+ searchmovies('')
  },[])
 
 
     return(
-        <div>
-            <h1>My First React App</h1>
+        <div className="app">
+            <h1>MovieX</h1>
+             
+              <div className="search">
+                <input
+                placeholder="Search for movie"
+                value={searchTerm}
+                onChange={(e)=>setsearchTerm(e.target.value)}
+                />
+               <img
+                    src={SearchIcon}
+                     alt="search"
+                    onClick={() => searchmovies(searchTerm)}
+                />
+              </div>
+              {
+                    movies?.length>0 ? (
+                    <div className="container">
+                        {movies.map((movie)=>(
+                            <MovieCard movie={movie}/>
+                        ))}
+                    </div>
+                    ) :
+                    <div className="empty">
+                        <h2>Movie not found</h2>
+                    </div>
+                }
+
         </div>   
     );
 }
